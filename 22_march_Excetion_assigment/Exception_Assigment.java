914e1e147e1a8512e60485468ab0f0e0e045ab96
GAUTAM CHAUDHARY <122784792+letscodewithgautam@users.noreply.github.com>
2023-03-22 16:11:43 +0530

    Add files via upload

---
+++ 22_march_Excetion_assigment/Exception_Assigment.java
@@ -0,0 +1,47 @@
+package march_22_23_exception;
+
+public class Exception_Assigment {
+		public static void main(String[] args) {
+			try {
+				try {
+					// Arithmetic exception
+					int i =10/0;
+					System.out.println(i);
+					}
+					catch(Exception e) {
+					System.out.println(e);
+					}
+				finally {
+					System.out.println("1st finally block after arithmetic exception ");
+				}
+					// null pointer exception
+					try {
+						String s = null;
+						System.out.println(s.length());
+					}
+					catch(Exception e) {
+						System.out.println(e);
+					}
+					finally {
+						System.out.println("2nd finally block after null pointer exception");
+					}
+					// ArrayIndexoutofbound exception
+					try {
+						int arr [] = new int[5];
+						arr[7]=10;
+					}
+					catch(Exception e) {
+						System.out.println(e);
+					}
+					finally {
+						System.out.println("3rd finally block after array index out of bound exception");
+					}
+				}
+				catch(Exception e) {
+					System.out.println(e);		
+				}
+			finally {
+				System.out.println("Differernt unchecked exceptions done");
+			}
+		}
+}
